0d176added741f8c71a1722a3de54afdf8caf601
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-05-13 00:34:47 +0530

    resolved compiledebug bug

--- lib/screens/feelings_history.dart
+++ lib/screens/feelings_history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class FeelingsHistory extends StatefulWidget {
@@ -11,9 +12,60 @@ class FeelingsHistory extends StatefulWidget {
 class _FeelingsHistoryState extends State<FeelingsHistory> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: HexColor("#FBFBFC"),
-      body: Container(),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: HexColor("#FBFBFC"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: IconButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        icon: const Icon(Icons.arrow_back_ios,
+                            color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.06,
+                    ),
+                    const Text(
+                      "Your Feelings History",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "SFPro",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "Your feelings from last 30 days",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontFamily: "SFPro",
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
